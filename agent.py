c11358ba0c6a5f6e2b66b69900fb83e72cd757cc
gs6029907@gmail.com <gs6029907@gmail.com>
2026-07-07 20:16:21 +0530

    feat: changes in agent

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ import random
 OPT_YES = 1
 OPT_NO = 2
 OPT_CARD = 3
-OPT_NUMBER = 0      # ADD THIS LINE -- was missing, causes empty on type-0 prompts
+OPT_NUMBER = 0
 OPT_PLAY = 7
 OPT_ATTACH = 8
 OPT_EVOLVE = 9
@@ -14,27 +14,26 @@ OPT_ATTACK = 13
 OPT_END = 14
 
 _DECK = [
-    65, 65, 65, 65,
-    878, 878, 878, 878,
-    1122, 1122, 1122, 1122,
-    1171, 1171, 1171, 1171,
-    1152, 1152, 1152, 1152,
-    1086, 1086, 1086, 1086,
-    1227, 1227, 1227, 1227,
-    1255, 1255, 1255, 1255,
-    19, 19, 19, 19,
-    11, 11, 11, 11,
-    66, 66, 66,
-    1097, 1097, 1097,
-    1115, 1115, 1115,
-    879, 879,
-    304, 304,
-    1210, 1210,
-    1182, 1182,
-    1194, 1194,
+    65,65,65,65,
+    878,878,878,878,
+    1122,1122,1122,1122,
+    1171,1171,1171,1171,
+    1152,1152,1152,1152,
+    1086,1086,1086,1086,
+    1227,1227,1227,1227,
+    1255,1255,1255,1255,
+    19,19,19,19,
+    11,11,11,11,
+    66,66,66,
+    1097,1097,1097,
+    1115,1115,1115,
+    879,879,
+    304,304,
+    1210,1210,
+    1182,1182,
+    1194,1194,
     12,
 ]
-
 
 def _finalize_choice(preferred_indexes, options, max_count):
     chosen = []
@@ -49,84 +48,54 @@ def _finalize_choice(preferred_indexes, options, max_count):
                 break
     return chosen[:max_count]
 
+def score_option(opt, obs_dict):
+    """Heuristic scoring for each option"""
+    t = opt.get("type")
+    score = 0
 
-def random_agent(obs_dict, config=None):
-    if obs_dict.get("select") is None:
-        return _DECK
-    select = obs_dict["select"]
-    options = select.get("option", [])
-    max_count = select.get("maxCount", 1)
-    if not options:
-        return []
-    return random.sample(range(len(options)), min(max_count, len(options)))
+    if t == OPT_ATTACK:
+        dmg = opt.get("damage", 0)
+        score += dmg
+        if opt.get("koPotential"):  # custom flag if available
+            score += 50
+    elif t == OPT_EVOLVE:
+        score += 30
+    elif t == OPT_ATTACH:
+        # Prefer attaching to active Pokémon
+        score += 20 if opt.get("inPlayArea") == 4 else 10
+    elif t == OPT_RETREAT:
+        # Retreat if active is low HP
+        active_hp = obs_dict.get("activeHP", 999)
+        if active_hp < 30:
+            score += 40
+    elif t == OPT_ABILITY:
+        score += 15
+    elif t == OPT_PLAY:
+        score += 10
+    elif t == OPT_END:
+        score -= 5  # discourage ending early
+    elif t == OPT_NO:
+        score -= 10
 
+    return score
 
 def agent(obs_dict, config=None):
     if obs_dict.get("select") is None:
         return _DECK
 
-    select = obs_dict["select"]
-    options = select.get("option", [])
-    max_count = select.get("maxCount", 1)
+    options = obs_dict["select"].get("option", [])
+    max_count = obs_dict["select"].get("maxCount", 1)
 
     if not options:
-        return _finalize_choice([], options, max_count)
+        return []
 
-    types = [opt.get("type") for opt in options]
+    # Score all options
+    scored = [(i, score_option(opt, obs_dict)) for i, opt in enumerate(options)]
+    scored.sort(key=lambda x: x[1], reverse=True)
 
-    # YES first -- handles opening hand, who-goes-first, ability confirmations
-    yes_indexes = [i for i, t in enumerate(types) if t == OPT_YES]
-    if yes_indexes:
-        return _finalize_choice([yes_indexes[0]], options, max_count)
+    # Pick best option(s), randomize among ties
+    best_score = scored[0][1]
+    best_indexes = [i for i, s in scored if s == best_score]
 
-    evolve_indexes = [i for i, t in enumerate(types) if t == OPT_EVOLVE]
-    if evolve_indexes:
-        return _finalize_choice([evolve_indexes[0]], options, max_count)
-
-    ability_indexes = [i for i, t in enumerate(types) if t == OPT_ABILITY]
-    if ability_indexes:
-        return _finalize_choice([ability_indexes[0]], options, max_count)
-
-    play_indexes = [i for i, t in enumerate(types) if t == OPT_PLAY]
-    if play_indexes:
-        return _finalize_choice([play_indexes[0]], options, max_count)
-
-    attach_options = [
-        (i, opt) for i, opt in enumerate(options)
-        if opt.get("type") == OPT_ATTACH
-    ]
-    if attach_options:
-        active_targets = [
-            idx for idx, opt in attach_options
-            if opt.get("inPlayArea") == 4
-        ]
-        if active_targets:
-            return _finalize_choice([active_targets[0]], options, max_count)
-        return _finalize_choice([attach_options[0][0]], options, max_count)
-
-    attack_indexes = [i for i, t in enumerate(types) if t == OPT_ATTACK]
-    if attack_indexes:
-        return _finalize_choice([attack_indexes[0]], options, max_count)
-
-    end_indexes = [i for i, t in enumerate(types) if t == OPT_END]
-    if end_indexes:
-        return _finalize_choice([end_indexes[0]], options, max_count)
-
-    retreat_indexes = [i for i, t in enumerate(types) if t == OPT_RETREAT]
-    if retreat_indexes:
-        return _finalize_choice([retreat_indexes[0]], options, max_count)
-
-    card_indexes = [i for i, t in enumerate(types) if t == OPT_CARD]
-    if card_indexes:
-        return _finalize_choice([card_indexes[0]], options, max_count)
-
-    discard_indexes = [i for i, t in enumerate(types) if t == OPT_DISCARD]
-    if discard_indexes:
-        return _finalize_choice([discard_indexes[0]], options, max_count)
-
-    no_indexes = [i for i, t in enumerate(types) if t == OPT_NO]
-    if no_indexes:
-        return _finalize_choice([no_indexes[0]], options, max_count)
-
-    # Catch-all including OPT_NUMBER (type 0) and any other unknown types
-    return _finalize_choice(list(range(len(options))), options, max_count)
+    chosen = random.choice(best_indexes)
+    return _finalize_choice([chosen], options, max_count)
